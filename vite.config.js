38501099e70033aa4d8a60015df4d672634a691f
aakashragu27 <aakashnadhan07@gmail.com>
2026-06-14 15:31:48 +0530

    Changes in deploy.yml file

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base : '/Cafeteria-Management-System/',
-  plugins: [react()],
-  server: {
-    watch: {
-      ignored: ['**/.vs/**', '**/node_modules/**', '**/dist/**', '**/.git/**']
-    }
-  }
+  plugins: [react()]
 });
